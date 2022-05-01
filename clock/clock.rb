5458776cd272ea834641f64e5ce699b749f5e654
grenzk <74033991+grenzk@users.noreply.github.com>
2022-05-01 11:22:11 +0800

    Refactor code

--- clock/clock.rb
+++ clock/clock.rb
@@ -11,35 +11,7 @@ class Clock
   end
 
   def to_s
-    hours_and_minutes
-    negative_hour_and_minutes
     Time.parse("#{@hour}:#{@minute}").strftime('%H:%M')
-  end
-
-  def hours_and_minutes
-    if @minute > 59 && @hour > 24
-      @hour = (@hour % 24) + (@minute / 60 % 24)
-    elsif @hour > 24
-      @hour = @hour % 24
-    elsif @minute > 59
-      @hour += @minute / 60 % 24
-    end
-  end
-
-  def negative_hour_and_minutes
-    if @minute < -59 && @hour < -24
-      @hour = ((-@hour % 24) - (@minute / 60 % 24)).abs
-    elsif @hour.negative? && @hour > -24
-      @hour = (24 + @hour).abs
-    elsif @minute.negative? && @minute > -59
-      @hour = @minute.abs / 60 % 24
-    elsif @hour < -24
-      @hour = @hour % 24
-    elsif @minute == -60
-      @hour += @minute / 60
-    elsif @minute < -59
-      @hour += @minute / 60 % 24
-    end
   end
 
   def +(other)
